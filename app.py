7b8930727b1aac6f670a9ca30c7ad853b4f92e1e
Nuno S. Osório <122723551+nunososorio@users.noreply.github.com>
2023-04-16 12:53:47 +0100

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,6 @@
 import streamlit as st
-import pandas as pd
 from pytube import YouTube
-import base64
-from io import BytesIO
+
 def main():
 	path = st.text_input('Enter URL of any youtube video')
 	option = st.selectbox(
@@ -15,12 +13,16 @@ def main():
 		st.write("Title of Video: " + str(video_object.title))
 		st.write("Number of Views: " + str(video_object.views))
 		if option=='audio':
-			video_object.streams.get_audio_only().download() 		#base64.b64encode("if file is too large").decode()	
+			video_object.streams.get_audio_only().download()
+			st.write("Download Link: " + str(video_object.streams.get_audio_only().url))
 		elif option=='highest_resolution':
 			video_object.streams.get_highest_resolution().download()
+			st.write("Download Link: " + str(video_object.streams.get_highest_resolution().url))
 		elif option=='lowest_resolution':
 			video_object.streams.get_lowest_resolution().download()
+			st.write("Download Link: " + str(video_object.streams.get_lowest_resolution().url))
 	if st.button("view"): 
 		st.video(path) 
+
 if __name__ == '__main__':
 	main()
